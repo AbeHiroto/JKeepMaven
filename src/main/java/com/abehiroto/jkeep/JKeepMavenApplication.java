834eedfea4e972afd0947d92f59d02004438ccd8
AbeHiroto <info.violasolo@gmail.com>
2025-03-28 14:55:23 +0900

    first commit

---
+++ src/main/java/com/abehiroto/jkeep/JKeepMavenApplication.java
@@ -0,0 +1,13 @@
+package com.abehiroto.jkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JKeepMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JKeepMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abehiroto/jkeep/JKeepMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abehiroto.jkeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JKeepMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
