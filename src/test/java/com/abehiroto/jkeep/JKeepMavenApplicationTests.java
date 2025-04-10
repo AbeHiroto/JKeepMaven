d06eaeea17f88866b7466ae4b5c5e041d65ba9cb
AbeHiroto <info.violasolo@gmail.com>
2025-04-10 15:28:18 +0900

    fixes for GitHubActions

--- src/test/java/com/abehiroto/jkeep/JKeepMavenApplicationTests.java
+++ src/test/java/com/abehiroto/jkeep/JKeepMavenApplicationTests.java
@@ -2,8 +2,10 @@ package com.abehiroto.jkeep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JKeepMavenApplicationTests {
 
 	@Test
