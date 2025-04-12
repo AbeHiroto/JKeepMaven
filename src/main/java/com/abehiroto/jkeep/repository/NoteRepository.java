8bbb3842bfd527925d7ea12c33f3b1c8124c49da
AbeHiroto <info.violasolo@gmail.com>
2025-04-12 16:00:38 +0900

    modify incrementAllOrdersByUser

--- src/main/java/com/abehiroto/jkeep/repository/NoteRepository.java
+++ src/main/java/com/abehiroto/jkeep/repository/NoteRepository.java
@@ -1,15 +1,25 @@
 package com.abehiroto.jkeep.repository;
 
-import com.abehiroto.jkeep.bean.Note;
-import com.abehiroto.jkeep.bean.User;
+// import jakarta.transaction.Transactional;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+// ※誤ったインポート！！※import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import com.abehiroto.jkeep.bean.Note;
+import com.abehiroto.jkeep.bean.User;
+
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    // 基本的なCRUD操作はJpaRepositoryが自動提供
-    // 例: findAll(), save(), deleteById()など
-	// ユーザーに紐づくノートをorderの昇順で取得
+
+    // 一括更新クエリ
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Note n SET n.order = n.order + 1 WHERE n.user.id = :userId")
+    int incrementAllOrdersByUser(@Param("userId") Long userId);
+    
     List<Note> findByUserOrderByOrderAsc(User user);
 }
